b9272387a0ee54fe6aeec2e924699e27b39ce40c
Solenyai <christianerasmo13@gmail.com>
2026-03-04 12:38:16 +0800

    Add Clarity identify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Clarity from '@microsoft/clarity'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    Clarity.identify("user-12345")
+  }, [])
 
   return (
     <>
