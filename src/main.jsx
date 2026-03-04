6fb8577c9eb4eb1a6f72655fa91aa74895ff95f4
Solenyai <christianerasmo13@gmail.com>
2026-03-04 12:30:22 +0800

    Update Clarity project ID

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Clarity from '@microsoft/clarity'
+
+// 🔴 PUT YOUR REAL PROJECT ID HERE
+Clarity.init("vqbknbxpnc")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
